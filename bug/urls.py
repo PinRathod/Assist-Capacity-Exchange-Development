7a1d99374503887723bb7eff540c71782d099077
PinRathod <pinrth2015@gmail.com>
2023-10-15 19:15:35 +0530

    Successfully created views and templates

--- bug/urls.py
+++ bug/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.bug, name="bug"),
+    path("register_bug/", views.register_bug, name="register_bug"),
+    path("list_bug/", views.list_bug, name="list_bug"),
+    path('view_bug/<int:id>/', views.view_bug, name='view_bug'),
 ]
--- bug/views.py
+++ bug/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.template import loader
+from .models import Bug
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 
 def bug(request):
     return HttpResponse("Welcome to bug app.")
+
+def register_bug(request):
+    if request.method == 'POST':
+        bug_des=request.POST['text']
+        bug_type=request.POST['bug_type']
+        report_date=request.POST['date']
+        status=request.POST['status']
+        add_bug=Bug(description=bug_des,bug_type=bug_type,report_date=report_date,status=status)
+        add_bug.save()
+        return redirect(list_bug)
+    return render(request, "register_bug.html")
+
+def list_bug(request):
+    bug_data = Bug.objects.all().values()
+    return render(request, 'list_bug.html', {'bug_data': bug_data})
+
+def view_bug(request,id):
+    data = get_object_or_404(Bug,id=id)
+    return render(request, "view_bug.html", {"data": data})
+
+
+
+
+
